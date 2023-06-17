b533e1aea97689cb3ba347639ad1d2df2ce6eaea
DiegoMX <diego.perez@modular-mx.com>
2023-06-17 12:37:00 -0700

    unit testing and corrections due to unit testing

--- autosar/libraries/Bfx.h
+++ autosar/libraries/Bfx.h
@@ -8,7 +8,7 @@
  * Bfx routines specification specifies the functionality, API and the configuration of the AUTOSAR
  * library for BIT functionality dedicated to fixed-point arithmetic routines All bit functions are
  * re-entrant and can handle several simultaneous requests from the application.
- * 
+ *
  * @reqs    SWS_Bfx_00220, SWS_Bfx_00222, SWS_Bfx_00200, SWS_Bfx_00201
  */
 #ifndef BFX_H
--- autosar/libraries/Bfx_16bits.h
+++ autosar/libraries/Bfx_16bits.h
@@ -8,9 +8,9 @@
  * Bfx routines specification specifies the functionality, API and the configuration of the AUTOSAR
  * library for BIT functionality dedicated to fixed-point arithmetic routines All bit functions are
  * re-entrant and can handle several simultaneous requests from the application.
- * 
+ *
  * @reqs    SWS_Bfx_00223, SWS_Bfx_00203, SWS_Bfx_00205, SWS_Bfx_00206, SWS_Bfx_00207, SWS_Bfx_00208
- *          SWS_Bfx_00209, SWS_Bfx_00212, SWS_Bfx_00213, SWS_Bfx_00214, 
+ *          SWS_Bfx_00209, SWS_Bfx_00212, SWS_Bfx_00213, SWS_Bfx_00214,
  */
 #include "Std_Types.h"
 
@@ -28,7 +28,7 @@
  *
  * @param[inout]  Data Pointer to input data
  * @param[in]  BitPn Bit position
- * 
+ *
  * @reqs    SWS_Bfx_00001, SWS_Bfx_00002, SWS_Bfx_00008
  */
 static inline void Bfx_SetBit_u16u8( uint16 *Data, uint8 BitPn )
@@ -50,7 +50,7 @@ static inline void Bfx_SetBit_u16u8( uint16 *Data, uint8 BitPn )
  *
  * @param[inout]  Data Pointer to inpur data
  * @param[in]  BitPn Bit position
- * 
+ *
  * @reqs    SWS_Bfx_00010, SWS_Bfx_00011, SWS_Bfx_00015
  */
 static inline void Bfx_ClrBit_u16u8( uint16 *Data, uint8 BitPn )
@@ -73,7 +73,7 @@ static inline void Bfx_ClrBit_u16u8( uint16 *Data, uint8 BitPn )
  * @param[in] BitPn Bit position
  *
  * @retval Boolean Bit status
- * 
+ *
  * @reqs    SWS_Bfx_00016, SWS_Bfx_00017, SWS_Bfx_00020
  */
 static inline boolean Bfx_GetBit_u16u8_u8( uint16 Data, uint8 BitPn )
@@ -133,7 +133,7 @@ static inline void Bfx_SetBits_u16u8u8u8( uint16 *Data, uint8 BitStartPn, uint8 
  * @param[in]  BitLn Bit field length
  *
  * @retval uint8 Bit field sequence
- * 
+ *
  * @reqs    SWS_Bfx_00028, SWS_Bfx_00029, SWS_Bfx_00034
  */
 static inline uint16 Bfx_GetBits_u16u8u8_u16( uint16 Data, uint8 BitStartPn, uint8 BitLn )
@@ -161,7 +161,7 @@ static inline uint16 Bfx_GetBits_u16u8u8_u16( uint16 Data, uint8 BitStartPn, uin
  *
  * @param[inout] Data Pointer to input Data
  * @param[in] Mask Mask used to set bits
- * 
+ *
  * @reqs    SWS_Bfx_00035, SWS_Bfx_00036, SWS_Bfx_00038
  */
 static inline void Bfx_SetBitMask_u16u16( uint16 *Data, uint16 Mask )
@@ -184,10 +184,10 @@ static inline void Bfx_SetBitMask_u16u16( uint16 *Data, uint16 Mask )
  *
  * @param[inout] Data Pointer to input Data
  * @param[in] Mask Mask value
- * 
+ *
  * @reqs    SWS_Bfx_00039, SWS_Bfx_00040, SWS_Bfx_00045
  */
-static inline void Bfx_ClrBitMask_u16u16( uint16 *Data, uint8 Mask )
+static inline void Bfx_ClrBitMask_u16u16( uint16 *Data, uint16 Mask )
 {
     *Data &= ~Mask;
 }
@@ -208,7 +208,7 @@ static inline void Bfx_ClrBitMask_u16u16( uint16 *Data, uint8 Mask )
  * @param[in] Mask Mask value
  *
  * @retval Boolean Value
- * 
+ *
  * @reqs    SWS_Bfx_00046, SWS_Bfx_00047, SWS_Bfx_00050
  */
 static inline boolean Bfx_TstBitMask_u16u16_u8( uint16 Data, uint16 Mask )
@@ -232,7 +232,7 @@ static inline boolean Bfx_TstBitMask_u16u16_u8( uint16 Data, uint16 Mask )
  * @param[in] Mask Mask value
  *
  * @retval Boolean Value
- * 
+ *
  * @reqs    SWS_Bfx_00051, SWS_Bfx_00055
  */
 static inline boolean Bfx_TstBitLnMask_u16u16_u8( uint16 Data, uint16 Mask )
@@ -255,7 +255,7 @@ static inline boolean Bfx_TstBitLnMask_u16u16_u8( uint16 Data, uint16 Mask )
  * @param[in] Data Input Data
  *
  * @retval Boolean Value
- * 
+ *
  * @reqs    SWS_Bfx_00056, SWS_Bfx_00060
  */
 static inline boolean Bfx_TstParityEven_u16_u8( uint16 Data )
@@ -285,12 +285,12 @@ static inline boolean Bfx_TstParityEven_u16_u8( uint16 Data )
  *      @endcode
  *
  * @param[inout] Data Pointer to input Data
- * 
+ *
  * @reqs    SWS_Bfx_00061, SWS_Bfx_00065
  */
 static inline void Bfx_ToggleBits_u16( uint16 *Data )
 {
-    *Data ^= 0xFFu;
+    *Data ^= 0xFFFFu;
 }
 
 /**
@@ -307,7 +307,7 @@ static inline void Bfx_ToggleBits_u16( uint16 *Data )
  *
  * @param[inout] Data Pointer to input Data
  * @param[in] Mask Mask
- * 
+ *
  * @reqs    SWS_Bfx_00066, SWS_Bfx_00069
  */
 static inline void Bfx_ToggleBitMask_u16u16( uint16 *Data, uint16 Mask )
@@ -331,7 +331,7 @@ static inline void Bfx_ToggleBitMask_u16u16( uint16 *Data, uint16 Mask )
  *
  * @param[inout] Data Pointer to input Data
  * @param[in] ShiftCnt Shift right count
- * 
+ *
  * @reqs    SWS_Bfx_00070, SWS_Bfx_00075
  */
 static inline void Bfx_ShiftBitRt_u16u8( uint16 *Data, uint8 ShiftCnt )
@@ -355,7 +355,7 @@ static inline void Bfx_ShiftBitRt_u16u8( uint16 *Data, uint8 ShiftCnt )
  *
  * @param[inout] Data Pointer to input Data
  * @param[in] ShiftCnt Shift left count
- * 
+ *
  * @reqs    SWS_Bfx_00076, SWS_Bfx_00080
  */
 static inline void Bfx_ShiftBitLt_u16u8( uint16 *Data, uint8 ShiftCnt )
@@ -382,12 +382,12 @@ static inline void Bfx_ShiftBitLt_u16u8( uint16 *Data, uint8 ShiftCnt )
  *
  * @param[inout] Data Pointer to input Data
  * @param[in] ShiftCnt Shift right count
- * 
+ *
  * @reqs    SWS_Bfx_00086, SWS_Bfx_00090
  */
 static inline void Bfx_RotBitRt_u16u8( uint16 *Data, uint8 ShiftCnt )
 {
-    *Data = ( *Data << ( 8u - ShiftCnt ) ) | ( *Data >> ShiftCnt );
+    *Data = ( *Data << ( 16u - ShiftCnt ) ) | ( *Data >> ShiftCnt );
 }
 
 /**
@@ -409,12 +409,12 @@ static inline void Bfx_RotBitRt_u16u8( uint16 *Data, uint8 ShiftCnt )
  *
  * @param[inout] Data Pointer to input Data
  * @param[in] ShiftCnt Shift left count
- * 
+ *
  * @reqs    SWS_Bfx_00095, SWS_Bfx_00098
  */
 static inline void Bfx_RotBitLt_u16u8( uint16 *Data, uint8 ShiftCnt )
 {
-    *Data = ( *Data >> ( 8u - ShiftCnt ) ) | ( *Data << ShiftCnt );
+    *Data = ( *Data >> ( 16u - ShiftCnt ) ) | ( *Data << ShiftCnt );
 }
 
 /**
@@ -434,7 +434,7 @@ static inline void Bfx_RotBitLt_u16u8( uint16 *Data, uint8 ShiftCnt )
  * @param[in] DestinationPosition Destination position
  * @param[in] SourceData Source data
  * @param[in] SourcePosition Source position
- * 
+ *
  * @reqs    SWS_Bfx_00101, SWS_Bfx_00108
  */
 static inline void Bfx_CopyBit_u16u8u16u8( uint16 *DestinationData, uint8 DestinationPosition, uint16 SourceData, uint8 SourcePosition )
@@ -466,7 +466,7 @@ static inline void Bfx_CopyBit_u16u8u16u8( uint16 *DestinationData, uint8 Destin
  * @param[in] BitStartPn Start bit position
  * @param[in] BitLn Bit field length
  * @param[in] Pattern Pattern to be set
- * 
+ *
  * @reqs    SWS_Bfx_00110, SWS_Bfx_00112
  */
 static inline void Bfx_PutBits_u16u8u8u16( uint16 *Data, uint8 BitStartPn, uint8 BitLn, uint16 Pattern )
@@ -493,7 +493,7 @@ static inline void Bfx_PutBits_u16u8u8u16( uint16 *Data, uint8 BitStartPn, uint8
  * @param[inout] Data Pointer to input data
  * @param[in] Pattern Pattern to be set
  * @param[in] Mask Mask value
- * 
+ *
  * @reqs    SWS_Bfx_00120, SWS_Bfx_00124
  */
 static inline void Bfx_PutBitsMask_u16u16u16( uint16 *Data, uint16 Pattern, uint16 Mask )
@@ -517,7 +517,7 @@ static inline void Bfx_PutBitsMask_u16u16u16( uint16 *Data, uint16 Pattern, uint
  * @param[inout] Data Pointer to destination data
  * @param[in] BitPn Destination position
  * @param[in] Status Source data
- * 
+ *
  * @reqs    SWS_Bfx_00130, SWS_Bfx_00132
  */
 static inline void Bfx_PutBit_u16u8u8( uint16 *Data, uint8 BitPn, boolean Status )
@@ -556,25 +556,90 @@ static inline void Bfx_PutBit_u16u8u8( uint16 *Data, uint8 BitPn, boolean Status
  * @param[in] Data Input value
  *
  * @retval Shifted and saturated bit pattern.
- * 
+ *
  * @reqs    SWS_Bfx_91002, SWS_Bfx_00134, SWS_Bfx_00135
  */
-static inline sint16 Bfx_ShiftBitSat_s16s8_s16( sint16 ShiftCnt, sint8 Data )
+static inline sint16 Bfx_ShiftBitSat_s16s8_s16( sint16 Data, sint8 ShiftCnt )
 {
-    sint16 Shifted;
+    sint16 result;
 
-    if( ShiftCnt < 0 )
+    if( ShiftCnt >= 0 )
     {
-        Shifted = Data >> ( ShiftCnt * -1 );
+        result = Data << ShiftCnt; /*Shift left*/
+        /*Check for saturation */
+        if( ( ( Data < 0 ) && ( result > Data ) ) || ( ( Data >= 0 ) && ( result < Data ) ) )
+        {
+            if( Data < 0 )
+            {
+                result = INT16_MIN;
+            }
+            else
+            {
+                result = INT16_MAX;
+            }
+        }
     }
     else
     {
-        Shifted = Data << ShiftCnt;
+        result = Data >> ( -ShiftCnt ); /* Shift right */
+        /* Fill vacated bits with the sign bit */
+        if( Data < 0 )
+        {
+            result |= (sint16)( 0xFFFF << ( 16 + ShiftCnt ) );
+        }
     }
 
-    return Shifted;
+    return result;
 }
 
+/**
+ * @brief  **16 bit Arithmetic shift with saturation**
+ *
+ * For signed data an arithmetic shift is performed. The vacated bits are filled with zeros
+ * and the result is saturated if its sign bit differs from the sign bits that are shifted out.
+ *
+ * If the shift count is less than zero, right-shift the value in Data by the absolute value of
+ * the shift count. The vacated bits are filled with the sign-bit (the most significant bit) and
+ * bits shifted out are discarded.
+ *
+ * Note that a shift right by the word width leaves all zeros or all ones in the result, de-
+ * pending on the sign-bit.
+ *
+ * **Example:**
+ *      @code
+ *      a 32 bit signed integer: The range for shift count is -32 to +31, allowing a shift left
+ *      up to 31 bit positions and a shift right up to 32 bit positions (a shift right by 32
+ *      bits leaves all zeros or all ones in the result, depending on the sign bit)
+ *      @endcode
+ *
+ * @param[in] ShiftCnt Shift count (-MaxShiftRight ... -1: right, 1 ... MaxShiftLeft:left)
+ * @param[in] Data Input value
+ *
+ * @retval Shifted and saturated bit pattern.
+ *
+ * @reqs    SWS_Bfx_91002, SWS_Bfx_00134, SWS_Bfx_00135
+ */
+static inline uint16 Bfx_ShiftBitSat_u16s8_u16( uint16 Data, sint8 ShiftCnt )
+{
+    uint16 result;
+
+    if( ShiftCnt >= 0 )
+    {
+        result = Data << ShiftCnt; /*Shift left */
+        /*Check for saturation */
+        if( result < Data )
+        {
+            result = UINT16_MAX; /*Saturate result */
+        }
+    }
+    else
+    {
+        result = Data >> ( -ShiftCnt );                /* Shift right */
+        result &= (uint16)( 0xFFFF >> ( -ShiftCnt ) ); /*Fill vacated bits with zero */
+    }
+
+    return result;
+}
 
 /**
  * @brief  **16 bit Count Leading Ones**
@@ -591,12 +656,12 @@ static inline sint16 Bfx_ShiftBitSat_s16s8_s16( sint16 ShiftCnt, sint8 Data )
  * @param[in] Data Input data
  *
  * @retval Bit position
- * 
+ *
  * @reqs    SWS_Bfx_91003, SWS_Bfx_00137
  */
 static inline uint8 Bfx_CountLeadingOnes_u16( uint16 Data )
 {
-    uint8 Count      = 0u;
+    uint8 Count       = 0u;
     uint16 BitChecker = 0x8000u;
 
     while( ( Data & BitChecker ) != 0u )
@@ -624,7 +689,7 @@ static inline uint8 Bfx_CountLeadingOnes_u16( uint16 Data )
  * @param[in] Data Input data
  *
  * @retval Bit position
- * 
+ *
  * @reqs    SWS_Bfx_91004, SWS_Bfx_00139
  */
 static inline uint8 Bfx_CountLeadingSigns_s16( sint16 Data )
@@ -658,12 +723,12 @@ static inline uint8 Bfx_CountLeadingSigns_s16( sint16 Data )
  * @param[in] Data Input data
  *
  * @retval Bit position
- * 
+ *
  * @reqs    SWS_Bfx_91005, SWS_Bfx_00141
  */
 static inline uint8 Bfx_CountLeadingZeros_u16( uint8 Data )
 {
-    uint8 Count      = 0u;
+    uint8 Count       = 0u;
     uint16 BitChecker = 0x8000u;
 
     while( ( Data & BitChecker ) == 0u )
